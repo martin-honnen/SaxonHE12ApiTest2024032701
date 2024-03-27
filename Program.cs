05c1980809728cea35cdd8fc7cb7ade9b83c4e7b
Martin Honnen <martin.honnen@gmx.de>
2024-03-27 18:48:39 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using net.sf.saxon.s9api;
+using net.liberty_development.SaxonHE12s9apiExtensions;
+//using System.Reflection;
+
+// force loading of updated xmlresolver (hopefully no longer needed with current release Saxon HE 12.4)
+//ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver"));
+//ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver_data"));
+
+var processor = new Processor(false);
+
+Console.WriteLine($"{processor.getSaxonEdition()} {processor.getSaxonProductVersion()}");
+
+var xslt30Transformer = processor.newXsltCompiler().Compile(new Uri("https://github.com/martin-honnen/martin-honnen.github.io/raw/master/xslt/processorTestHTML5Xslt3InitialTempl.xsl")).load30();
+
+xslt30Transformer.callTemplate(null, processor.NewSerializer(Console.Out));
